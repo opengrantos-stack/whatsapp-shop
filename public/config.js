d8db5d352f9e6c52176d17bfce4b254b0153613d
Opengrantos-stack <opengrant.os@gmail.com>
2026-08-22 18:23:51 +0100

    Update GC-AngGlobal WhatsApp number

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@ const lojaConfig = {
     nome: "GC-AngGlobal",
     descricao: "Comércio, compras e encomendas online.",
     logo: "png1.jpg",
-    numeroWhatsApp: "244923123456",
+    numeroWhatsApp: "244959674834",
     moeda: "Kz",
     mensagemPadrao: "Olá! Gostaria de fazer um pedido."
 };
